f91be9261680c72b2114f4a6e573742e8369769d
the_Ten_of_Hearts <ye_jin2004@163.com>
2025-09-24 13:20:24 +0800

    Fixed run script

--- run.ps1
+++ run.ps1
@@ -1,3 +1,7 @@
-docker rm -f gomoku-server
+# 尝试强制移除名为 gomoku-server 的旧容器，如果不存在则忽略错误
+try {
+    docker rm -f gomoku-server -ErrorAction Stop
+}
+catch {}
 
 docker run --rm -p 3000:3000 -v ./data:/app/data -v ./submissions:/app/submissions -v ./logs:/app/logs --name gomoku-server gomoku-platform
